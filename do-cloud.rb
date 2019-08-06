2284b68b133eaaeaaf56d8746137ef719614f01a
gabriel-aires <airesgabriel@gmail.com>
2019-08-06 19:05:18 -0300

    added ssh-key to instatiated vm

--- do-cloud.rb
+++ do-cloud.rb
@@ -1,24 +1,44 @@
+require 'droplet_kit'
+
+sshkey_file = "#{Dir.home}/.ssh/neocloud_id_rsa"
 token_file = ARGV.size == 1 ? ARGV.first : "STDIN"
 token = ARGF.gets.chomp
-puts "Token read from #{token_file}"
+puts "API Token read from #{token_file}"
 puts "-----------------------------"
 puts token
 
-require 'droplet_kit'
 cloud = DropletKit::Client.new(
 	access_token: 	token,
 	open_timeout: 	60,
 	timeout:	120,
 )
 
+if File.exists? sshkey_file
+	puts "Using existing ssh key at #{sshkey_file}"
+else
+	puts "Generating new ssh key-pair..."
+	system "ssh-keygen -t rsa -C neocloud -f #{sshkey_file} -N ''"
+end
 
+pubkey_content = File.read "#{sshkey_file}.pub"
+pubkey = DropletKit::SSHKey.new name: "neokey", public_key: pubkey_content
+key_uploaded = cloud.ssh_keys.all.any? { |key| key.name == "neokey" }
+
+if key_uploaded
+	puts "SSH key already uploaded to Digital Ocean"
+else
+	puts "Uploading key to Digital Ocean Account"
+	cloud.ssh_keys.create pubkey
+end
+
+neo_keys = cloud.ssh_keys.all.select { |key| key.name == "neokey" }.map { |key| key.fingerprint}
 template = DropletKit::Droplet.new(
-	name: 'neo1',
+	name: 'neo',
        	region: 'nyc3',
        	image: 'ubuntu-18-04-x64',
        	size: 's-1vcpu-1gb',
+	ssh_keys: neo_keys,
 )
 
+puts "Creating machine from template..."
 vm1 = cloud.droplets.create template
-puts vm1.name
-puts vm1.id
