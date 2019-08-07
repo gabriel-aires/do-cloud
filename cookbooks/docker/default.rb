e883efb11efc5b9b2bb86b8551c94b3ea8fec79a
gabriel-aires <airesgabriel@gmail.com>
2019-08-07 13:02:49 -0300

    bugfix

--- cookbooks/docker/default.rb
+++ cookbooks/docker/default.rb
@@ -6,5 +6,5 @@ end
 
 execute "docker run -d -p 8080:8080 --name whoami -t jwilder/whoami" do
   subscribes :run, "execute[docker pull jwilder/whoami]"
-  not_if ""
+  not_if "pgrep -f docker-proxy > /dev/null"
 end
