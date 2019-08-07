6351cdbf9d2554cc330c19eaa992efb3eb3b7bd3
gabriel-aires <airesgabriel@gmail.com>
2019-08-06 22:28:10 -0300

    get network information

--- do-cloud.rb
+++ do-cloud.rb
@@ -35,21 +35,19 @@ end
 
 neo_keys = cloud.ssh_keys.all.select { |key| key.name == "neokey" }.map { |key| key.fingerprint}
 
-#Configure Infrastructure
+#Provision Infrastructure
 config = {}
 
 vm_image = 'ubuntu-18-04-x64'
 vm_size = 's-1vcpu-1gb'
 
-config['active_lb']		= { name: 'neo-lb1', region: 'nyc1', image: vm_image, size: vm_size }
-config['standby_lb']	= { name: 'neo-lb2', region: 'nyc3', image: vm_image, size: vm_size }
-config['app_server1'] = { name: 'neo-app1', region: 'nyc1', image: vm_image, size: vm_size }
-config['app_server2'] = { name: 'neo-app2', region: 'nyc3', image: vm_image, size: vm_size }
+config['active_lb']		= { name: 'neo-lb1', region: 'nyc1', image: vm_image, size: vm_size}
+config['standby_lb']	= { name: 'neo-lb2', region: 'nyc3', image: vm_image, size: vm_size}
+config['app_server1'] = { name: 'neo-app1', region: 'nyc1', image: vm_image, size: vm_size}
+config['app_server2'] = { name: 'neo-app2', region: 'nyc3', image: vm_image, size: vm_size}
 
 config.each do |vm, settings|
 	puts "Creating machine #{vm}..."
-	puts "VM configuration:"
-	pp settings
 
 	host_config = DropletKit::Droplet.new(
 		name: settings[:name],
@@ -60,5 +58,12 @@ config.each do |vm, settings|
 		private_networking: true,
 	)
 
-	cloud.droplets.create host_config
+	created = cloud.droplets.create host_config
+	sleep 5
+
+	host = cloud.droplets.find(id: created.id)
+	host.networks.v4.each do |network|
+		puts "#{network.type} ipv4:\t#{network.ip_address}"
+	end
+
 end
