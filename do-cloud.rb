32f9a5ea01419ef83395065e6f71b847a98b9764
gabriel-aires <airesgabriel@gmail.com>
2019-08-06 21:37:24 -0300

    create load balancers and app servers

--- do-cloud.rb
+++ do-cloud.rb
@@ -1,17 +1,18 @@
 require 'droplet_kit'
 
-sshkey_file = "#{Dir.home}/.ssh/neocloud_id_rsa"
+#initialize Digital Ocean client
 token_file = ARGV.size == 1 ? ARGV.first : "STDIN"
 token = ARGF.gets.chomp
-puts "API Token read from #{token_file}"
-puts "-----------------------------"
-puts token
+puts "API Token read from #{token_file}. Initializing client..."
 
 cloud = DropletKit::Client.new(
 	access_token: 	token,
 	open_timeout: 	60,
 	timeout:	120,
 )
+
+#SSH key-pair generation
+sshkey_file = "#{Dir.home}/.ssh/neocloud_id_rsa"
 
 if File.exists? sshkey_file
 	puts "Using existing ssh key at #{sshkey_file}"
@@ -20,6 +21,7 @@ else
 	system "ssh-keygen -t rsa -C neocloud -f #{sshkey_file} -N ''"
 end
 
+#SSH public key upload
 pubkey_content = File.read "#{sshkey_file}.pub"
 pubkey = DropletKit::SSHKey.new name: "neokey", public_key: pubkey_content
 key_uploaded = cloud.ssh_keys.all.any? { |key| key.name == "neokey" }
@@ -32,13 +34,31 @@ else
 end
 
 neo_keys = cloud.ssh_keys.all.select { |key| key.name == "neokey" }.map { |key| key.fingerprint}
-template = DropletKit::Droplet.new(
-	name: 'neo',
-       	region: 'nyc3',
-       	image: 'ubuntu-18-04-x64',
-       	size: 's-1vcpu-1gb',
-	ssh_keys: neo_keys,
-)
 
-puts "Creating machine from template..."
-vm1 = cloud.droplets.create template
+#Configure Infrastructure
+config = {}
+
+vm_image = 'ubuntu-18-04-x64'
+vm_size = 's-1vcpu-1gb'
+
+config['active_lb']		= { name: 'neo-lb1', region: 'nyc1', image: vm_image, size: vm_size }
+config['standby_lb']	= { name: 'neo-lb2', region: 'nyc3', image: vm_image, size: vm_size }
+config['app_server1'] = { name: 'neo-app1', region: 'nyc1', image: vm_image, size: vm_size }
+config['app_server2'] = { name: 'neo-app2', region: 'nyc3', image: vm_image, size: vm_size }
+
+config.each do |vm, settings|
+	puts "Creating machine #{vm}..."
+	puts "VM configuration:"
+	pp settings
+
+	host_config = DropletKit::Droplet.new(
+		name: settings[:name],
+		region: settings[:region],
+		image: settings[:image],
+		size: settings[:size],
+		ssh_keys: neo_keys,
+		private_networking: true,
+	)
+
+	cloud.droplets.create host_config
+end
