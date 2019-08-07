bcd2e4a1f2c0d37024a0c8b2c3c114a0f942f721
gabriel-aires <airesgabriel@gmail.com>
2019-08-07 13:37:41 -0300

    use default http port for containers

--- cookbooks/docker/default.rb
+++ cookbooks/docker/default.rb
@@ -4,7 +4,7 @@ execute "docker pull jwilder/whoami" do
   subscribes :run, "package[docker.io]"
 end
 
-execute "docker run -d -p 8080:8080 --name whoami -t jwilder/whoami" do
+execute "docker run -d -p 80:8000 --name whoami -t jwilder/whoami" do
   subscribes :run, "execute[docker pull jwilder/whoami]"
   not_if "pgrep -f docker-proxy > /dev/null"
 end
