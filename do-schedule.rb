52dd1861a0442dde16f21e17a8ced4cbb9323625
gabriel-aires <airesgabriel@gmail.com>
2019-08-07 22:54:04 -0300

    updated crontab installer

--- do-schedule.rb
+++ do-schedule.rb
@@ -3,9 +3,10 @@
 up_at			= "0  7    * * 1-5"
 down_at		= "0  19   * * 1-5"
 run_as		= " root"
-up_cmd 		= "    cd /opt/do-cloud && ./do-cloud.rb < .do-token.txt >> /var/log/cloud-up.log"
-down_cmd	= "    cd /opt/do-cloud && ./do-destroy.rb < .do-token.txt >> /var/log/cloud-down.log"
+up_cmd 		= "    cd /opt/do-cloud && ./do-cloud.rb < .do-token.txt &>> /var/log/cloud-up.log"
+down_cmd	= "    cd /opt/do-cloud && ./do-destroy.rb < .do-token.txt &>> /var/log/cloud-down.log"
 testfile	= "/opt/do-cloud/.do-crontab.txt"
+message   = "Crontab installed at #{Time.now.to_s}"
 
 if File.exist? testfile
 	puts "Crontab already installed."
@@ -13,13 +14,15 @@ if File.exist? testfile
 else
 
 	File.open testfile, 'w' do |file|
-		file.puts "crontab installed at #{Time.now.to_s}"
+		file.puts message
 	end
 
 	File.open "/etc/crontab", 'a' do |file|
 		file.puts up_at + run_as + up_cmd
 		file.puts down_at + run_as + down_cmd
 	end
+
+  puts message
 
 end
 
