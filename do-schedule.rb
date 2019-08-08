7360dee43fbd94cdee69a31150860e62a50495a9
gabriel-aires <airesgabriel@gmail.com>
2019-08-07 21:17:43 -0300

    added script for crontab installation

---
+++ do-schedule.rb
@@ -0,0 +1,25 @@
+#!/usr/bin/env ruby
+
+up_at			= "0  7    * * 1-5"
+down_at		= "0  19   * * 1-5"
+run_as		= " root"
+up_cmd 		= "    cd /opt/do-cloud && ./do-cloud.rb < .do-token.txt >> /var/log/cloud-up.log"
+down_cmd	= "    cd /opt/do-cloud && ./do-destroy.rb < .do-token.txt >> /var/log/cloud-down.log"
+testfile	= "/opt/do-cloud/.do-crontab.txt"
+
+if File.exist? testfile
+	puts "Crontab already installed."
+
+else
+
+	File.open testfile, 'w' do |file|
+		file.puts "crontab installed at #{Time.now.to_s}"
+	end
+
+	File.open "/etc/crontab", 'a' do |file|
+		file.puts up_at + run_as + up_cmd
+		file.puts down_at + run_as + down_cmd
+	end
+
+end
+
